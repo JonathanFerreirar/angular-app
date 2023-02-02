3a9e7dd35f0e7d1989e5e0a614c12f868b2954eb
jhowf <jonathanferreirar@gmail.com>
2023-02-02 15:20:20 -0300

    componentsCreats

---
+++ src/app/painel/painel.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-painel',
+  templateUrl: './painel.component.html',
+  styleUrls: ['./painel.component.css'],
+})
+export class PainelComponent implements OnInit {
+  ngOnInit(): void {}
+}
--- src/app/top/top.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-top',
-  templateUrl: './top.component.html',
-  styleUrls: ['./top.component.css'],
-})
-export class TopComponent {}
