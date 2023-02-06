8f0275a99926e4063a89331c95c0a06dac2f6933
jhowf <jonathanferreirar@gmail.com>
2023-02-06 17:54:43 -0300

    Adding several functions

--- src/app/progresso/progresso.component.ts
+++ src/app/progresso/progresso.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progresso',
@@ -8,7 +8,5 @@ import { Component, OnInit } from '@angular/core';
 export class ProgressoComponent implements OnInit {
   ngOnInit(): void {}
 
-  public progressoNumber: number = 50;
-
-  public progresso: string = `width: ${this.progressoNumber}%`;
+  @Input() public progressoNumber: number = 0;
 }
--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Coracao } from '../shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css'],
 })
-export class TentativasComponent implements OnInit {
-  public coracaoVazio: string = '../../assets/img/empty.png';
-  public coracaoCheio: string = '../../assets/img/full.png';
+export class TentativasComponent implements OnInit, OnChanges {
+  @Input() public tentativas!: number;
 
-  constructor() {}
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true),
+  ];
+
+  constructor() {
+    console.log(this.coracoes);
+  }
+  ngOnChanges() {
+    if (this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas;
+      this.coracoes[indice - 1].cheio = false;
+    }
+    console.log(`Tentivas recebidas do painel:`, this.tentativas);
+  }
+
   ngOnInit(): void {}
 }
